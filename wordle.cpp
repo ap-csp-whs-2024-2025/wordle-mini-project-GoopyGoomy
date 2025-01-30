6db9d4c17e050b4cd321f21dbabce8e0913c690b
GoopyGoomy <earlgreytea0627@gmail.com>
2025-01-30 21:59:12 +0000

    a

--- wordle.cpp
+++ wordle.cpp
@@ -12,13 +12,21 @@
 #include <vector>    // std::vector
 
 std::vector<int> createSecret() {
-    // Write this procedure here
-
-    return {};    // replace this with your code
+    std::vector<int> code = {};
+    int counter = 0;
+    while (counter < 4) //REPEAT 4 TIMES
+    {
+        int val = rand() % 10; //random  number between 0 and 9
+        code.push_back(val); //append (code, val); This is pushing back val into the list code
+        counter = counter + 1;
+    }
+    return code;    
 }
 
 std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> guess) {
     // Write this procedure here
+    //maybe make a list for the X's and O's
+    //go down the list of randomied numbers and do if statements
     return {"X", "X", "X", "X"};    // replace this with your code
 }
 
@@ -37,7 +45,7 @@ int main()
     srand(time(0)); // seeds our random number generator
     int random_num = rand() % 10;  // random number between 0 and 9
     
-    std::vector<int> secret_code = createSecret();
+    std::vector<int> secret_code = createSecret(4);
     std::vector<int> user_guess = {};
     std::vector<std::string> hint = {};    // an empty list
 
